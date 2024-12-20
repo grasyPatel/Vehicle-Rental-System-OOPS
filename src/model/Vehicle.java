2482ccbc8f3e33000ec24159974d4c8aa18393ea
grasypatel <gracepatel91@gmail.com>
2024-12-20 08:01:30 +0530

    --

--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Vehicle {
+    int id;
+    String type;
+    String brand;
+    String model;
+    double rentalPricePerDay;
+    boolean available;
+
+    public Vehicle() {
+    }
+
+    public int getId() {
+        return id;
+    }
+    public boolean checkAvailability(){
+        return available;
+
+    }
+    public void toggleAvailability(){
+
+    }
+    public void  displayDetails(){
+
+    }
 }
--- src/service/VehicleService.java
+++ src/service/VehicleService.java
@@ -1,27 +1,5 @@
 package service;
 
 public class VehicleService {
-    int id;
-    String type;
-    String brand;
-    String model;
-    double rentalPricePerDay;
-    boolean available;
 
-    public VehicleService() {
-    }
-
-    public int getId() {
-        return id;
-    }
-    public boolean checkAvailability(){
-        return available;
-
-    }
-    public void toggleAvailability(){
-
-    }
-    public void  displayDetails(){
-
-    }
 }
